76ff2a88789a028f71684a1bf9a631dd2471380d
AnOpenSauceDev <119765865+AnOpenSauceDev@users.noreply.github.com>
2023-10-24 18:02:06 +1000

    Fetch Linux distro via `/etc/os-release`

--- src/main/java/com/github/anopensaucedev/libmcdevfabric/OSUtils.java
+++ src/main/java/com/github/anopensaucedev/libmcdevfabric/OSUtils.java
@@ -7,16 +7,25 @@ import java.nio.charset.StandardCharsets;
 
 public class OSUtils {
 
-    private static String LSB_REMOVAL_PATTERN = "Description:\t";
+    private static String LSB_REMOVAL_PATTERN = "Description:\t"; // we don't use lsb_release, because most distros either a) don't have it, or b) allow /etc/os-release to be accessed.
+
 
     //fetch distribution here.
     public static String fetchOSData(){
 
         try {
-            // this should work on: Arch Linux, Debian + derivatives. NOTE: not all distro's, (especially older ones) support this
-            Process data = Runtime.getRuntime().exec("lsb_release -d");
-            String osdata = new String(data.getInputStream().readAllBytes(),StandardCharsets.UTF_8); // We now have our OS data, but it has annoying fluff still.
-            return osdata.replace(LSB_REMOVAL_PATTERN,""); // remove "Description:\t", so we only have the distro name (\t is the tab escape code)
+
+            Process data = Runtime.getRuntime().exec("cat /etc/os-release");
+            String osdata = new String(data.getInputStream().readAllBytes(),StandardCharsets.UTF_8);
+            String[] osrelease = osdata.split("\\r?\\n"); // so we can actually read the lines
+            String osReleaseName;
+            for (int x = 0; x < osrelease.length; x++){
+                if(osrelease[x].contains("NAME")){
+                    osReleaseName = osrelease[x];
+                    return osReleaseName.substring(5).replace("\"", ""); // remove quotes
+                }
+            }
+            return "Generic Linux";
         } catch (IOException e) {
             e.printStackTrace();
             return "Unknown OS"; // fail "gracefully"
