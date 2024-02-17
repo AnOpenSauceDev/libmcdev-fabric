e2358eb3df8e3e17fe2aece15d205f6120c013ac
AnOpenSauceDev <noreply@anopensauce.dev>
2024-02-17 15:26:50 +1000

    update camera stuff

--- src/main/java/com/github/anopensaucedev/libmcdevfabric/render/CameraUtils.java
+++ src/main/java/com/github/anopensaucedev/libmcdevfabric/render/CameraUtils.java
@@ -2,11 +2,46 @@ package com.github.anopensaucedev.libmcdevfabric.render;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Camera;
+import net.minecraft.command.argument.EntityAnchorArgumentType;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MarkerEntity;
+import net.minecraft.util.math.Vec3d;
+import org.joml.Quaterniondc;
+import org.joml.Vector3d;
+import org.joml.Vector3f;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CameraUtils {
 
     public static void FocusCameraAtPoint(Camera camera){
+
+    }
+
+    /**
+     * focuses the camera towards the player, at the mean of where all entities are.
+     */
+    public static void CreateMeanOfEntities(MinecraftClient client, List<Entity> entities){
+        if (client.world != null) {
+            MarkerEntity temp = new MarkerEntity(EntityType.MARKER,client.world);
+
+            Vec3d pos = new Vec3d(0,0,0);
+
+            for (int i = 0; i < entities.size(); i++) {
+                pos.add(entities.get(i).getPos());
+            }
+
+            Vec3d meanPos = new Vec3d(pos.x/entities.size(), pos.y/entities.size(), pos.z/entities.size());
+
+
+
+            temp.setPos(meanPos.x,meanPos.y,meanPos.z);
+            temp.lookAt(EntityAnchorArgumentType.EntityAnchor.EYES,client.player.getEyePos());
+
+            client.world.addEntity(temp);
+        }
 
     }
 
